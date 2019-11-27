177226fd374df20d622ceaf42952b0d78b9c6462
Sergey Khrebtov <sergey.khrebtov@sibers.com>
2019-11-27 18:25:26 +0700

    added change

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Navbar from './components/layout/navbar'
-import Dashboard from './components/dashboard/dashboard'
+import Navbar from './components/layout/navbar';
+import Dashboard from './components/dashboard/dashboard';
+import ProjecDetails from './components/projects/project-detail'
+import ProjectDetails from './components/projects/project-detail';
+import SignIn from './components/auth/sign-in';
+import SignUp from './components/auth/sign-up';
 
 function App() {
   return (
@@ -9,7 +13,10 @@ function App() {
       <div className="App">
         <Navbar />
         <Switch>
-          <Route path="/" component={Dashboard} />
+          <Route path='/' component={Dashboard} exact />
+          <Route path='/project/:id' component={ProjectDetails} />
+          <Route path='/signin' component={SignIn} />
+          <Route path='/signup' component={SignUp} />
         </Switch>
       </div>
     </BrowserRouter>
--- src/components/projects/project-list.js
+++ src/components/projects/project-list.js
@@ -1,31 +1,12 @@
 import React from 'react';
+import ProjectSummary from './project-summary';
 
 const ProjectList = () => {
     return (
         <div className="project-list section">
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by Net Ninja</p>
-                    <p className="grey-text">3rd September, 2am</p>
-                </div>
-            </div>
-
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by Net Ninja</p>
-                    <p className="grey-text">3rd September, 2am</p>
-                </div>
-            </div>
-
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by Net Ninja</p>
-                    <p className="grey-text">3rd September, 2am</p>
-                </div>
-            </div>
+            <ProjectSummary />
+            <ProjectSummary />
+            <ProjectSummary />
         </div>
     );
 };
