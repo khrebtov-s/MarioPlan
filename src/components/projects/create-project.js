9f119e6026d17ddef933540f2b2156bc9c4af268
Sergey Khrebtov <sergey.khrebtov@sibers.com>
2019-11-29 16:11:29 +0700

    routing and changed css

--- src/components/projects/create-project.js
+++ src/components/projects/create-project.js
@@ -22,7 +22,7 @@ export class CreateProject extends Component {
             <div className="container">
                 <form onSubmit={this.handleSubmit} className="white">
                     <h5 className="grey-text text-darken-3">
-                        Sign In
+                        Create New Project
                     </h5>
                     <div className="input-field">
                         <label htmlFor="title">Title</label>
