0307cadbebe31496b6c6a953eeba4cd1bab69a84
Sergey Khrebtov <sergey.khrebtov@sibers.com>
2019-12-03 17:46:55 +0700

    firebase auth with redux

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,7 +1,8 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import SignedInLinks from './SignedInLinks'
-import SignedOutLinks from './SignedOutLinks'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import SignedInLinks from './SignedInLinks';
+import SignedOutLinks from './SignedOutLinks';
+import { connect } from 'react-redux';
 
 const Navbar = () => {
   return (
@@ -12,7 +13,14 @@ const Navbar = () => {
         <SignedOutLinks />
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+const mapStateToProps = (state) => {
+  console.log(state);
+  return {
+
+  };
+};
+
+export default connect(mapStateToProps)(Navbar);
