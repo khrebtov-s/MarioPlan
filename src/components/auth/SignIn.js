d1341c9124f1f14d16c214c9cd49f5e4b5728f00
Sergey Khrebtov <sergey.khrebtov@sibers.com>
2019-12-04 16:56:41 +0700

    added logging users in

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,20 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { signIn } from '../../store/actions/authActions';
 
 class SignIn extends Component {
   state = {
     email: '',
     password: ''
-  }
+  };
+
   handleChange = (e) => {
     this.setState({
       [e.target.id]: e.target.value
     })
-  }
+  };
+
   handleSubmit = (e) => {
     e.preventDefault();
-    console.log(this.state);
-  }
+    this.props.signIn(this.state);
+  };
+
   render() {
+    const { authError } = this.props;
+
     return (
       <div className="container">
         <form className="white" onSubmit={this.handleSubmit}>
@@ -29,11 +36,26 @@ class SignIn extends Component {
           </div>
           <div className="input-field">
             <button className="btn pink lighten-1 z-depth-0">Login</button>
+            <div className="red-text center">
+              {authError ? <p>{authError}</p> : null}
+            </div>
           </div>
         </form>
       </div>
-    )
+    );
+  };
+};
+
+const mapStateToProps = (state) => {
+  return {
+    authError: state.auth.authError
   }
 }
 
-export default SignIn
+const mapDispatchToProps = (dispatch) => {
+  return {
+    signIn: (creds) => dispatch(signIn(creds))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignIn);
