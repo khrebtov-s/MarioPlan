dce6c7d3a0a4fb3a30c32eec90d088dd124e2bc7
Sergey Khrebtov <sergey.khrebtov@sibers.com>
2019-12-09 14:51:31 +0700

    added routing guarding

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 class SignUp extends Component {
   state = {
@@ -6,17 +8,23 @@ class SignUp extends Component {
     password: '',
     firstName: '',
     lastName: '',
-  }
+  };
+
   handleChange = (e) => {
     this.setState({
       [e.target.id]: e.target.value
     })
-  }
+  };
+
   handleSubmit = (e) => {
     e.preventDefault();
     console.log(this.state);
-  }
+  };
+
   render() {
+    const { auth } = this.props;
+    if (auth.uid) return <Redirect to="/" />
+
     return (
       <div className="container">
         <form className="white" onSubmit={this.handleSubmit}>
@@ -43,7 +51,13 @@ class SignUp extends Component {
         </form>
       </div>
     )
-  }
-}
+  };
+};
 
-export default SignUp
+const mapStateToProps = (state) => {
+  return {
+    auth: state.firebase.auth
+  };
+};
+
+export default connect(mapStateToProps)(SignUp);
--- src/components/projects/CreateProject.js
+++ src/components/projects/CreateProject.js
@@ -1,24 +1,29 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { createProject } from '../../store/actions/projectActions'
-
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { createProject } from '../../store/actions/projectActions';
+import { Redirect } from 'react-router-dom';
 class CreateProject extends Component {
   state = {
     title: '',
     content: ''
-  }
+  };
+
   handleChange = (e) => {
     this.setState({
       [e.target.id]: e.target.value
     })
-  }
+  };
+
   handleSubmit = (e) => {
     e.preventDefault();
-    // console.log(this.state);
     this.props.createProject(this.state);
     this.props.history.push('/');
-  }
+  };
+
   render() {
+    const { auth } = this.props;
+    if (!auth.uid) return <Redirect to="/signin" />
+
     return (
       <div className="container">
         <form className="white" onSubmit={this.handleSubmit}>
@@ -36,14 +41,20 @@ class CreateProject extends Component {
           </div>
         </form>
       </div>
-    )
-  }
-}
+    );
+  };
+};
+
+const mapStateToProps = (state) => {
+  return {
+    auth: state.firebase.auth
+  };
+};
 
 const mapDispatchToProps = dispatch => {
   return {
     createProject: (project) => dispatch(createProject(project))
-  }
-}
+  };
+};
 
-export default connect(null, mapDispatchToProps)(CreateProject)
+export default connect(mapStateToProps, mapDispatchToProps)(CreateProject);
